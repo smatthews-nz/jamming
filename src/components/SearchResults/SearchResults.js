c536e6f36d2ea77e0625b23d7d37861c72664bb6
Sam Matthews <sam.matthews@gettimely.com>
2021-08-22 20:54:40 +1200

    Add initial static components

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+import './SearchResults.css';
+
+class SearchResults extends Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* <!-- Add a TrackList component --> */}
+            </div>
+        )
+    }
+}
+
+export default SearchResults;
