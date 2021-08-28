2335ccbac519e498ce75bbd8e1dccfc2d17ae1b0
Samm <sam@subsupps.co.nz>
2021-08-28 12:31:02 +1200

    being implementing search logic

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,11 +3,33 @@ import { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state ={
+            searchTerm: ''
+        }
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search(){
+        this.props.onSearch(this.state.searchTerm);
+    }
+
+    handleTermChange(event){
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         )
     }
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -55,6 +55,8 @@ class App extends Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track){
@@ -76,12 +78,21 @@ class App extends Component {
     this.setState({playlistTracks: updatedTracks})
   }
 
+  savePlaylist(){
+    //generate track uris
+    let trackURIs = this.state.playlistTracks.map( track => track.uri)
+  }
+
+  search(term){
+    console.log(term)
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults 
               searchResults={this.state.searchResults} 
@@ -91,6 +102,7 @@ class App extends Component {
               name={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
